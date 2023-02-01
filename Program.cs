544a8701590af7ebceab96df2ac6b211e29c3c00
Alex Isaak <gafar@xaker.ru>
2023-02-01 19:13:40 +0300

    itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace tri_chisla
+{
+    class Program
+    {
+        static void Main()
+        {
+Console.Write("Ведите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите третье число: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double max = 0;
+
+if (a > b)
+{
+    if (a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+}
+else
+{
+    if (b > c)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+}
+
+Console.Write("Максимальное число: " + max);
+        }
+        }
+}
